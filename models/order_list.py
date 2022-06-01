bc859ff7a259f859800cf7d9b53305cf88a24ab4
sandyysh221 <102677016+sandyysh221@users.noreply.github.com>
2022-06-01 18:05:24 +0100

    orders created

---
+++ models/order_list.py
@@ -0,0 +1,6 @@
+from models.order import *
+import datetime
+
+order1 = Order("Ariel", datetime.date(2022, 5, 30), "tuna mayo", 1)
+order2 = Order("Mulan", datetime.date(2022, 5, 31), "kimchi cheese", 1)
+order3 = Order("Belle", datetime.date(2022, 6, 1), "chicken katsu", 1)
