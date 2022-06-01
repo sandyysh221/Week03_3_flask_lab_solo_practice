e26582205f1fcae4e3046c7f19af0e85204620f9
sandyysh221 <102677016+sandyysh221@users.noreply.github.com>
2022-06-01 18:02:35 +0100

    order class complete

---
+++ models/order.py
@@ -0,0 +1,6 @@
+class Order:
+    def __init__(self, customer_name, order_date, filling, quantity):
+        self.name = customer_name
+        self.date = order_date
+        self.filling = filling
+        self.quantity = quantity
